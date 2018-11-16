d10a3fafe25fc9da5dc2065a02135e2d726cdd80
Pawar <E0404074@eaton.ad.etn.com>
2018-11-16 16:23:16 +0530

    Adding the weather API.

--- index.js
+++ index.js
@@ -13,19 +13,77 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+const host = 'api.worldweatheronline.com';
+const wwoApiKey = '82227a3f61af4d6aaed94721181611';
+
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+
+  let city = req.body.queryResult.parameters['geo-city']; // city is a required param
+
+  // Get the date for the weather forecast (if present)
+  let date = '';
+  if (req.body.queryResult.parameters['date']) {
+    date = req.body.queryResult.parameters['date'];
+    console.log('Date: ' + date);
+  }
+
+  // Call the weather API
+  callWeatherApi(city, date).then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': `I don't know the weather but I hope it's good!` });
   });
+
+  // var speech =
+  //   req.body.result &&
+  //   req.body.result.parameters &&
+  //   req.body.result.parameters.echoText
+  //     ? req.body.result.parameters.echoText
+  //     : "Seems like some problem. Speak again.";
+  // return res.json({
+  //   speech: speech,
+  //   displayText: speech,
+  //   source: "webhook-echo-sample"
+  // });
 });
+
+function callWeatherApi (city, date) {
+  return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+    console.log('API Request: ' + host + path);
+
+    // Make the HTTP request to get the weather
+    http.get({host: host, path: path}, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let forecast = response['data']['weather'][0];
+        let location = response['data']['request'][0];
+        let conditions = response['data']['current_condition'][0];
+        let currentConditions = conditions['weatherDesc'][0]['value'];
+
+        // Create response
+        let output = `Current conditions in the ${location['type']} 
+        ${location['query']} are ${currentConditions} with a projected high of
+        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+        ${forecast['date']}.`;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling the weather API: ${error}`)
+        reject();
+      });
+    });
+  });
+}
 
 restService.post('/webhook',function(req,res){ 
 
